8431e0f8e69a2438c4b0b30b68763f662c7570b7
TaeroHwang <htr0414@gmail.com>
2023-02-25 14:28:21 +0900

    add last steps of Python_beginning

---
+++ 23.02.25.py
@@ -0,0 +1,17 @@
+# import time
+# print(time.localtime())
+# print(time.strftime("%Y-%m-%d %H:%M:%S"))
+
+# import datetime
+# print("오늘 날짜는 ", datetime.date.today())
+# today = datetime.date.today()
+# td = datetime.timedelta(days=100)
+# print("우리가 만난지 100일은", today + td)
+
+# 퀴즈 10
+# import byme
+# byme.sign()
+
+# import travel.thailand
+# trip_to = travel.thailand.ThailandPackage()
+# trip_to.detail()
